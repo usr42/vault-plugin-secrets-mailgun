370bc96697f0df40812e44a3f4f4821e9dedfc87
usr42 <usr42@users.noreply.github.com>
2018-12-24 00:06:27 +0100

    Add initial config path

---
+++ plugin/path_config.go
@@ -0,0 +1,117 @@
+package mgsecret
+
+import (
+	"context"
+	"github.com/hashicorp/vault/logical"
+	"github.com/hashicorp/vault/logical/framework"
+)
+
+// TODO: Add TTL?
+func pathConfig(b *backend) *framework.Path {
+	return &framework.Path{
+		Pattern: "config",
+		Fields: map[string]*framework.FieldSchema{
+			"api_key": {
+				Type:        framework.TypeString,
+				Description: `Mailgun API Key`,
+			},
+			"domain": {
+				Type:        framework.TypeString,
+				Description: "Domain to generate SMTP credentials for",
+			},
+		},
+
+		Operations: map[logical.Operation]framework.OperationHandler{
+			logical.ReadOperation: &framework.PathOperation{
+				Callback: b.pathConfigRead,
+				Summary:  "Return the current Mailgun configuration.",
+			},
+			logical.UpdateOperation: &framework.PathOperation{
+				Callback: b.pathConfigWrite,
+				Summary:  "Configure the Mailgun settings.",
+			},
+		},
+
+		HelpSynopsis:    pathConfigHelpSyn,
+		HelpDescription: pathConfigHelpDesc,
+	}
+}
+
+func (b *backend) pathConfigRead(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
+	cfg, err := getConfig(ctx, req.Storage)
+	if err != nil {
+		return nil, err
+	}
+	if cfg == nil {
+		return nil, nil
+	}
+
+	return &logical.Response{
+		Data: map[string]interface{}{
+			"domain":  cfg.Domain,
+		},
+	}, nil
+}
+
+func (b *backend) pathConfigWrite(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
+	cfg, err := getConfig(ctx, req.Storage)
+	if err != nil {
+		return nil, err
+	}
+	if cfg == nil {
+		cfg = &config{}
+	}
+
+	apiKey, ok := data.GetOk("api_key")
+	if ok {
+		cfg.ApiKey = apiKey.(string)
+	}
+
+	domain, ok := data.GetOk("domain")
+	if ok {
+		cfg.Domain = domain.(string)
+	}
+
+	entry, err := logical.StorageEntryJSON("config", cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := req.Storage.Put(ctx, entry); err != nil {
+		return nil, err
+	}
+
+	return nil, nil
+}
+
+type config struct {
+	ApiKey string
+	Domain string
+}
+
+func getConfig(ctx context.Context, s logical.Storage) (*config, error) {
+	var cfg config
+	cfgRaw, err := s.Get(ctx, "config")
+	if err != nil {
+		return nil, err
+	}
+	if cfgRaw == nil {
+		return nil, nil
+	}
+
+	if err := cfgRaw.DecodeJSON(&cfg); err != nil {
+		return nil, err
+	}
+
+	return &cfg, err
+}
+
+const pathConfigHelpSyn = `
+Configure the Mailgun backend.
+`
+
+const pathConfigHelpDesc = `
+The Mailgun backend requires the Mailgun API key and the domain for SMTP 
+credentials. This endpoint is used to configure those credentials as well as 
+default values for the backend in general.
+`
